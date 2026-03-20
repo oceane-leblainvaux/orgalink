f7f0686cf4551d1f7a929638142524a8a827f1ef
oceane-leblainvaux <leblainvaux.oceane@icloud.com>
2026-03-20 10:32:58 +0100

    mise a jour du config

--- php/config.php
+++ php/config.php
@@ -1,13 +1,19 @@
 <?php
-$host = "localhost"; 
-$dbname = "projet_m1"; 
-$username = "root"; 
-$password = ""; 
-
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Erreur de connexion : " . $e->getMessage());
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+if (file_exists(__DIR__ . '/db_config.php')) {
+    require_once __DIR__ . '/db_config.php';
+} else {
+    $host = "localhost"; 
+    $dbname = "projet_m1"; 
+    $username = "root"; 
+    $password = ""; 
+    try {
+        $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        die("Erreur de connexion : " . $e->getMessage());
+    }
 }
 ?>
