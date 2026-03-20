83b19f87886f85bed96d3e30d24853c7f2665015
oceane-leblainvaux <leblainvaux.oceane@icloud.com>
2026-03-20 11:05:30 +0100

    essaie

--- js/signup.js
+++ js/signup.js
@@ -6,19 +6,22 @@ function togglePassword(id) {
         input.type = "password";
     }
 }
+
 const openButton = document.querySelector("#openMenu");
 const dialog = document.querySelector("#menuDialog");
 
-openButton.addEventListener("click", () => {
-    if (!dialog.open) {
-        dialog.showModal();
-    } else {
-        dialog.close();
-    }
-});
+if (openButton && dialog) {
+    openButton.addEventListener("click", () => {
+        if (!dialog.open) {
+            dialog.showModal();
+        } else {
+            dialog.close();
+        }
+    });
 
-dialog.addEventListener("click", ({ target }) => {
-    if (target.nodeName === "DIALOG") {
-        dialog.close("dismiss");
-    }
-});
+    dialog.addEventListener("click", ({ target }) => {
+        if (target.nodeName === "DIALOG") {
+            dialog.close("dismiss");
+        }
+    });
+}
