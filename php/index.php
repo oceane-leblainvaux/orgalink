3b6721b4cca2b5e4d155a73a8e4a1a9d19982d1b
oceane-leblainvaux <leblainvaux.oceane@icloud.com>
2026-03-24 15:29:43 +0100

    DEBUGUER LE PHP

--- php/index.php
+++ php/index.php
@@ -3,11 +3,11 @@ session_start();
 
 // Si l'utilisateur est co, on redirige vers page d'accueil
 if (isset($_SESSION['id_utilisateur'])) {
-    header("Location: index.html");
+    header("Location: ../html/index.html");
     exit;
 } else {
     // Sinonil va vers la page de login
-    header("Location: login.html");
+    header("Location: ../html/login.html");
     exit;
 }
 ?>
