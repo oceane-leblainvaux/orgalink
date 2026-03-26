7083e6f36e0858030c0caa357b22e99ef04b2233
Faustine <faustine.madec@gmail.com>
2026-03-26 09:24:18 +0100

    chnagement

--- php/save_graph_jason.php
+++ php/save_graph_jason.php
@@ -11,7 +11,7 @@ if (!isset($_GET['nom'])) {
 $nom = basename($_GET['nom']);
 $nom = pathinfo($nom, PATHINFO_FILENAME); // enlève .png si le nom contient .png
 
-$json_path = "../schemas/json/" . $nom . ".json";
+$json_path = "../schemas/" . $nom . ".json";
  
 if (!file_exists($json_path)) {
     echo json_encode(["success" => false, "message" => "Fichier JSON introuvable."]);
